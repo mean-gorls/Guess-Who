c013779fdeeb7b1cb4ee1b98d7c5103a8f2cec5c
austin <austin.wood1776@live.com>
2019-03-13 10:08:05 -0700

    Added html and js for the game page

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict' 
+
+//Needs more arguments
+function Person(name,hair,glasses,){
+    this.name = name;
+    this.hair = hair;
+    this.glasses = glasses;
+}
+
+//Do we want to do our class for the people? 
+var Shannon = new Person('Shannon','Brown',true);
+var Eric = new Person('Eric','Black',false);
+var Adrianna = new Person('Adrianna','Black',true);
+var Jonny = new Person('Jonny','Brown',true);
+var Ryan = new Person('Ryan','Other',true);
+var Austin = new Person('Austin','Brown',false);
